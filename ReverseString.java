e3c25ef3d62fafa9654ab79c1d1154ca4cb5ea7b
Gen <genalyn.estrada14@gmail.com>
2017-03-21 19:53:21 +0000

    Final solution

---
+++ ReverseString.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public String reverseString(String s) {
+        StringBuilder reversedString = new StringBuilder(s);
+        return reversedString.reverse().toString();
+    }
+}
